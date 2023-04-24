5e4f50c54ec7fcb76a0f4e18fbf42bf2fb62eb95
CHANDRASHEKAR S <58178421+cs-victorious@users.noreply.github.com>
2023-04-24 20:17:52 +0530

    updated Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 // Your First Program
 
-class Hello {
+public class Hello {
     public static void main(String[] args) {
         System.out.println("Logged in to Github"); 
       System.out.println("Hello, World!"); 
